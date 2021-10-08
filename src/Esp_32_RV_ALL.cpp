25cc2eef21ddff5de6e696216b53d1105aca7495
hoalods@gmail.com <hoalods@gmail.com>
2021-10-07 23:12:36 -0500

    Entrega proyecto 3

--- src/Esp_32_RV_ALL.cpp
+++ src/Esp_32_RV_ALL.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#include "Adafruit_VL53L0X.h"
+//#include "Adafruit_VL53L0X.h"
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -10,16 +10,19 @@
 #include <Servo.h>
 #include "Wire.h"
 
+#include <VL53L0X.h>
+
 // Direction I2C
 #define MPU 0x68 // IMU
 
 // Conversions
-#define A_R 16384.0            // 32768/2
-#define G_R 131.0              // 32768/250
-#define RAD_TO_DEG = 57.295779 // 180/PI
+#define A_R 16384.0          // 32768/2
+#define G_R 131.0            // 32768/250
+#define RAD_TO_DEG 57.295779 // 180/PI
 
 /*  Objects  */
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+//Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+VL53L0X sensor;
 TaskHandle_t Task1; // for other core
 Servo servo1;
 BluetoothSerial SerialBT;
@@ -50,13 +53,28 @@ BluetoothSerial SerialBT;
 #define echoPin 4
 
 /* Variables */
-// Servo
-int distance;
-long duration;
-
-// US
+// Servo and IR
+float distance;
+float Xobs[7];
+float Yobs[7];
+float XobsIn[7];
+float YobsIn[7];
+float Obs_x;
+float Obs_y;
+float Obs_x_In;
+float Obs_y_In;
+float Uaox;
+float Uaoy;
+float Uaox_n;
+float Uaoy_n;
+float Obs_alpha;
+float Obs_alpha_Env;
+float Obs_x_Env;
+float Obs_y_Env;
 int posDegrees;
 bool turnChange = true;
+bool any = true;
+size_t contObs = 0;
 
 // Directions Motor
 int forward[2] = {0, 1};
@@ -86,6 +104,9 @@ const int resolution = 10;
 // MPU-6050
 int16_t AcX, AcY, GyY, GyZ;
 
+float Ac_Y_ini = 0;
+float Gy_Z_ini = 0;
+
 float Acc[2];
 float Wp_MPU;
 float Gy_Z;
@@ -93,8 +114,10 @@ float Gy_Y;
 float Ac_X;
 float Ac_Y;
 float Angle_Z;
+float Angle_Y;
 float Max_Angle_Y;
 float Max_Vel_X;
+float Vel_Y_MPU;
 float Vel_Y;
 float Vp_MPU;
 float Post_X;
@@ -150,16 +173,34 @@ float tR;
 float tL;
 size_t contL = 1;
 size_t contR = 1;
-size_t cont = 1;
+size_t cont = 2;
 
 float Wr = 0;
 float Wl = 0;
-float x_MPU[3000];
-float y_MPU[3000];
-float theta_MPU[3000];
-float x_Enc[3000];
-float y_Enc[3000];
-float theta_Enc[3000];
+
+float Xg = 70;
+float Yg = 50;
+float Ugtgx;
+float Ugtgy;
+float Ugtgx_n; //Vector normalized
+float Ugtgy_n;
+float alpha;
+float Xfin;
+float Yfin;
+float d[2000];
+float theta_g[2000];
+float Vp_d[2000];
+float Wp_d[2000];
+float Err_ant_Vp;
+float Err_ant_ang;
+float Err_ant2_ang;
+
+float x_MPU[2000];
+float y_MPU[2000];
+float theta_MPU[2000];
+float x_Enc[2000];
+float y_Enc[2000];
+float theta_Enc[2000]; //angulo del carro
 
 long prevTime;
 long prevTimeEnc;
@@ -167,10 +208,9 @@ long prevTimeMPU;
 float dt;
 
 // Time between action (milliseconds)
-int laserPeriod = 200;
-int servoPeriod = 200;
+int servoPeriod = 70;
 int mpuPeriod = 10;
-int carControlPeriod = 30;
+int carControlPeriod = 150;
 int motorControlPeriod = 10;
 
 /* Function declaration */
@@ -186,6 +226,7 @@ void f_stop();
 void clear();
 void encoder();
 float processEnc(bool enc_antA, bool encA, bool enc_antB, bool encB, int now_microSeg);
+void calibration();
 
 void setup()
 {
@@ -196,11 +237,13 @@ void setup()
   Wire.endTransmission(true);
 
   Serial.begin(1500000);
-  Serial.println("VL53L0X test");
-  if (!lox.begin())
+
+  while (!sensor.init())
   {
-    Serial.println(F("Error al iniciar VL53L0X"));
+    Serial.println("Failed to detect and initialize sensor of distance!");
   }
+
+  sensor.startContinuous();
 
   xTaskCreatePinnedToCore(
       codeForTask1, // Task function.
@@ -242,51 +285,95 @@ void setup()
   enc_antLB = digitalRead(left_B);
   enc_antRA = digitalRead(rigth_A);
   enc_antRB = digitalRead(rigth_B);
+
+  calibration();
 }
 
 void loop()
 {
   long now = millis();
 
-  /* if (now - lastLaser > laserPeriod)
-  {
-    lastLaser = now;
-    VL53L0X_RangingMeasurementData_t measure;
-
-    lox.rangingTest(&measure, false); // si se pasa true como parametro, muestra por puerto serie datos de debug
-    if (measure.RangeStatus != 4)
-    {
-      distance = measure.RangeMilliMeter;
-      // Serial.print("Distancia (mm): "+distance);
-    }
-    else
-    {
-      // Serial.println(" 1280");
-      distance = 1280;
-    }
-  }
-  */
-
   if (now - lastServo > servoPeriod)
   {
-    lastServo = now;
-    if (turnChange)
+    if (any)
     {
-      posDegrees = posDegrees + 45;
-      if (posDegrees >= 180)
+      if (turnChange)
       {
-        turnChange = false;
+        posDegrees = posDegrees + 30;
+        contObs++;
+        if (posDegrees >= 180)
+        {
+          contObs = 6;
+          turnChange = false;
+        }
       }
+      else
+      {
+        posDegrees = posDegrees - 30;
+        contObs--;
+        if (posDegrees <= 0)
+        {
+          contObs = 0;
+          turnChange = true;
+        }
+      }
+      servo1.write(posDegrees);
     }
     else
     {
-      posDegrees = posDegrees - 45;
-      if (posDegrees <= 1)
+      distance = sensor.readRangeContinuousMillimeters();
+      distance = distance / 10;
+      if (distance > 50)
       {
-        turnChange = true;
+        distance = 50;
       }
+      else if (distance < 8)
+      {
+        distance = 0;
+      }
+      float posDegreesTemp = posDegrees - 90;
+
+      Xobs[contObs] = cos(posDegreesTemp * 0.0174532925) * distance;
+      Yobs[contObs] = sin(posDegreesTemp * 0.0174532925) * distance;
+
+      /*Serial.print("Xobs[");
+      Serial.print(contObs);
+      Serial.print("]: ");
+      Serial.print(Xobs[contObs]);
+      Serial.print("  Yobs[");
+      Serial.print(contObs);
+      Serial.print("]: ");
+      Serial.println(Yobs[contObs]);*/
+
+      Obs_x = 0.25 * Xobs[0] + 0.5 * Xobs[1] + 0.75 * Xobs[2] + Xobs[3] + 0.75 * Xobs[4] + 0.5 * Xobs[5] + 0.25 * Xobs[6];
+      Obs_y = 0.25 * Yobs[0] + 0.5 * Yobs[1] + 0.75 * Yobs[2] + Yobs[3] + 0.75 * Yobs[4] + 0.5 * Yobs[5] + 0.25 * Yobs[6];
+
+      Obs_x_In = cos(theta_Enc[cont - 1] * 0.0174532925) * Obs_x - sin(theta_Enc[cont - 1] * 0.0174532925) * Obs_y;
+      Obs_y_In = sin(theta_Enc[cont - 1] * 0.0174532925) * Obs_x + cos(theta_Enc[cont - 1] * 0.0174532925) * Obs_y;
+
+      Obs_alpha = atan2(Obs_y_In, Obs_x_In) * (180.0 / PI);
+
+      Uaox = Obs_x_In;
+      Uaoy = Obs_y_In;
+
+      Uaox_n = Uaox / sqrt(pow(Uaox, 2) + pow(Uaoy, 2));
+      Uaoy_n = Uaoy / sqrt(pow(Uaox, 2) + pow(Uaoy, 2));
+
+      /*Serial.print(" Obs: ");
+      Serial.print(Obs_alpha);
+      Serial.print(" Obs_x: ");
+      Serial.print(Obs_x);
+      Serial.print(" Obs_y: ");
+      Serial.println(Obs_y);
+
+      Serial.print("dist: ");
+      Serial.print(distance);
+      Serial.print(" ang ");
+      Serial.println(posDegrees - 90);*/
     }
-    servo1.write(posDegrees);
+
+    any = !any;
+    lastServo = now;
   }
 
   now = millis();
@@ -311,45 +398,158 @@ void loop()
     GyY = Wire.read() << 8 | Wire.read();
     GyZ = Wire.read() << 8 | Wire.read();
 
-    Ac_X = AcX / A_R;
-    Ac_Y = AcY / A_R;
-    Gy_Z = GyZ / G_R;
-    Gy_Y = GyY / G_R;
+    Ac_X = AcX * (9.81 / 16384.0);
+    Ac_Y = AcY * (9.81 / 16384.0);
+    Gy_Z = GyZ * (250.0 / 32768.0) * (3.14159 / 180);
+    Gy_Y = GyY * (250.0 / 32768.0) * (3.14159 / 180);
 
     dt = (millis() - prevTime) / 1000.0;
     prevTime = millis();
 
-    // Integrals to obtain moviment in X
-    float Vel_X = Ac_X * dt;
-
-    if (Vel_X > Max_Vel_X)
-    {
-      Max_Vel_X = Vel_X;
-    }
-
-    if (Ac_Y * dt > Max_Vel_X)
+    if (abs(Ac_Y * dt * 100) > Ac_Y_ini * 1.1)
     {
       // Integrals to obtain vel in Y
-      Vp_MPU += Ac_Y * dt; // X carro
+      Vel_Y_MPU = Ac_Y * dt * 100; // X carro
+      Vp_MPU += Vel_Y_MPU;
     }
+    Serial.print("VY: ");
+    Serial.print(Ac_Y * dt * 100);
+    Serial.print(" AcY: ");
+    Serial.print(Ac_Y);
+    Serial.print(" dt: ");
+    Serial.println(dt);
 
-    float Angle_Y = Gy_Y * dt;
+    Angle_Y = Gy_Y * dt;
 
-    if (Angle_Y > Max_Angle_Y)
+    if (abs(Angle_Y) > Max_Angle_Y)
     {
-      Max_Angle_Y = Angle_Y;
+      Max_Angle_Y = abs(Angle_Y);
     }
 
-    if (Gy_Z * dt > Max_Angle_Y)
+    if (abs(Gy_Z) > Max_Angle_Y)
     {
-      Wp_MPU += Gy_Z * dt;
+      Angle_Z = Gy_Z * dt;
+      Wp_MPU = Angle_Z;
     }
+
+    //MPU Position X,Y
+
+    dt = (millis() - prevTimeMPU) / 1000.0;
+    prevTimeMPU = millis();
+
+    float Vel_X_MPU = cos(Wp_MPU) * Vp_MPU;
+    float Vel_Y_MPU = sin(Wp_MPU) * Vp_MPU;
+
+    /*Serial.print("  Vp: ");
+    Serial.print(Vp_MPU);
+    Serial.print(" Vx: ");
+    Serial.print(Vel_X_MPU);
+    Serial.print(" Vy: ");
+    Serial.print(Vel_Y_MPU);*/
+
+    x_MPU[cont] = x_MPU[cont - 1] + dt * Vel_X_MPU;
+    y_MPU[cont] = y_MPU[cont - 1] + dt * Vel_Y_MPU;
+    theta_MPU[cont] = theta_MPU[cont - 1] + dt * Wp_MPU;
+
+    /*Serial.print("  x: ");
+    Serial.print(x_MPU[cont]);
+    Serial.print("  y: ");
+    Serial.print(y_MPU[cont]);
+    Serial.print("  tetaAct: ");
+    Serial.println(theta_MPU[cont]);*/
   }
 
   if (now - lastCarControl >= carControlPeriod)
   {
+
     lastCarControl = now;
 
+    dt = (millis() - prevTimeEnc) / 1000.0;
+    prevTimeEnc = millis();
+
+    //Encoder Position X,Y
+
+    Ugtgx = Xg - x_Enc[cont - 1];
+    Ugtgy = Yg - y_Enc[cont - 1];
+
+    Ugtgx_n = Ugtgx / sqrt(pow(Ugtgx, 2) + pow(Ugtgy, 2));
+    Ugtgy_n = Ugtgy / sqrt(pow(Ugtgx, 2) + pow(Ugtgy, 2));
+
+    d[cont] = sqrt(pow(Ugtgx, 2) + pow(Ugtgy, 2));
+    //theta_g[cont] = atan2(Ugtgy, Ugtgx) * (180 / PI);
+
+    //CONDICIONALES THETA
+
+    alpha = 0.5;
+    Xfin = alpha * Uaox_n + (1 - alpha) * Ugtgx_n;
+    Yfin = alpha * Uaoy_n + (1 - alpha) * Ugtgy_n;
+
+    if (Xobs[3] > 25)
+    {
+      theta_g[cont] = atan2(Ugtgy, Ugtgx) * (180 / PI);
+      //Serial.print(" Xobs[3] 1 ");
+    }
+    else if (Xobs[3] < 15)
+    {
+      theta_g[cont] = Obs_alpha;
+      //Serial.print(" Xobs[3] 3 ");
+    }
+    else
+    {
+      theta_g[cont] = atan2(Yfin, Xfin);
+      //Serial.print(" Xobs[3] 2 : ");
+    }
+    /*Serial.print(Xobs[3]);
+    Serial.print(" posAng: ");
+    Serial.println(posDegrees);
+    Serial.print(" Theta_g: ");
+    Serial.println(theta_g[cont]);*/
+    //theta_g[cont] = Obs_alpha;
+    float kp = 0.006;
+    float ki = 0.002;
+    float kd = 0;
+
+    float b2 = kd / dt;
+    float b1 = -(2 * kd + kp * dt) / dt;
+    float b0 = (kd + kp * dt + pow(ki * dt, 2)) / dt;
+
+    float Err_ang = theta_g[cont] - theta_Enc[cont - 1];
+    Wp_d[cont] = Wp_d[cont - 1] + b0 * Err_ang + b1 * Err_ant_ang + b2 * Err_ant2_ang;
+
+    if (Wp_d[cont] > 10)
+    {
+      Wp_d[cont] = 10;
+    }
+    else if (Wp_d[cont] < -10)
+    {
+      Wp_d[cont] = -10;
+    }
+
+    kp = 0.02;
+    ki = 0.004;
+    kd = 0;
+
+    b2 = kd / dt;
+    b1 = -(2 * kd + kp * dt) / dt;
+    b0 = (kd + kp * dt + pow(ki * dt, 2)) / dt;
+
+    Vp_d[cont] = Vp_d[cont - 1] + b0 * d[cont] + b1 * d[cont - 1] + b2 * d[cont - 1];
+    //Vp_d[cont] = 0.8;
+
+    if (Vp_d[cont] > 12)
+    {
+      Vp_d[cont] = 12;
+    }
+    else if (Vp_d[cont] < -12)
+    {
+      Vp_d[cont] = -12;
+    }
+
+    if (d[cont] < 0.5)
+    {
+      Vp_d[cont] = 0;
+      Wp_d[cont] = 0;
+    }
     Wr = Wr / (carControlPeriod / motorControlPeriod);
     Wl = Wl / (carControlPeriod / motorControlPeriod);
 
@@ -359,39 +559,48 @@ void loop()
     float Vel_X_Enc = cos(theta_Enc[cont - 1] * 0.0174532925) * Vp_Enc;
     float Vel_Y_Enc = sin(theta_Enc[cont - 1] * 0.0174532925) * Vp_Enc;
 
-    dt = (millis() - prevTimeEnc) / 1000.0;
-    prevTimeEnc = millis();
+    x_Enc[cont] = x_Enc[cont - 1] + dt * Vel_X_Enc * 5.7;
+    y_Enc[cont] = y_Enc[cont - 1] + dt * Vel_Y_Enc * 4.8;
+    theta_Enc[cont] = theta_Enc[cont - 1] + dt * Wp_Enc * 60;
 
-    x_Enc[cont] = x_Enc[cont - 1] + dt * Vel_X_Enc * 6.41025641;
-    y_Enc[cont] = y_Enc[cont - 1] + dt * Vel_Y_Enc * 6.41025641;
-    theta_Enc[cont] = theta_Enc[cont - 1] + dt * Wp_Enc;
-    /*
-    Serial.print("Wr: ");
-    Serial.print(Wr);
-    Serial.print("  Vp: ");
-    Serial.print(Vp_Enc);
-    Serial.print("  VelX: ");
-    Serial.print(Vel_X_Enc);
-    Serial.print("  VelY: ");
-    Serial.print(Vel_Y_Enc);
-    Serial.print("  Wp: ");
-    Serial.print(Wp_Enc);
-    Serial.print("  x: ");
+    if (theta_Enc[cont] < -180)
+    {
+      theta_Enc[cont] = -180;
+    }
+    else if (theta_Enc[cont] > 180)
+    {
+      theta_Enc[cont] = 180;
+    }
+
+    /*Serial.print("Vp_d: ");
+    Serial.print(Vp_d[cont]);
+    Serial.print(" Wp_d: ");
+    Serial.print(Wp_d[cont]);
+    Serial.print(" Err_ang: ");
+    Serial.print(Err_ang);
+    Serial.print(" ThetaG: ");
+    Serial.print(theta_g[cont]);
+    Serial.print(" d: ");
+    Serial.print(d[cont]);
+    Serial.print(" x: ");
     Serial.print(x_Enc[cont]);
-    Serial.print("  y: ");
+    Serial.print(" y: ");
     Serial.print(y_Enc[cont]);
-    Serial.print("  tetaAct: ");
+    Serial.print(" theta: ");
     Serial.println(theta_Enc[cont]);*/
-    cont++;
+
+    Err_ant2_ang = Err_ant_ang;
+    Err_ant_ang = Err_ang;
     Wr = 0;
     Wl = 0;
+    cont++;
   }
 
   if (now - lastMotorControl >= motorControlPeriod)
   {
     lastMotorControl = now;
     // Control rueda derecha
-    Wd_encR = Vd / R + (Wd * L) / (2 * R);
+    Wd_encR = Vp_d[cont - 1] / R + (Wp_d[cont - 1] * L) / (2 * R);
     float Err_Wr = Wd_encR - W_encR;
     float salR = 2.1806 * Err_Wr - 1.9209 * Err_Wr_ant + Wr_ant;
     if (salR > 7)
@@ -421,13 +630,14 @@ void loop()
       //f_move_L(back, PWM);
     }
 
+    //Serial.print("Wr: ");
     //Serial.println(W_encR);
     //Serial.println(now);
     Wr_ant = salR;
     Err_Wr_ant = Err_Wr;
 
     // Control rueda izquierda
-    Wd_encL = Vd / R - (Wd * L) / (2 * R);
+    Wd_encL = Vp_d[cont - 1] / R - (Wp_d[cont - 1] * L) / (2 * R);
     float Err_Wl = Wd_encL * 1.15 - W_encL;
     float salL = 2.2043 * Err_Wl - 1.9347 * Err_Wl_ant + Wl_ant;
     if (salL > 7)
@@ -457,45 +667,15 @@ void loop()
       }
       //f_move_R(back, PWM);
     }
-    //Serial.print(Wd_encL);
-    //Serial.print("  ");
+
+    //Serial.print("Wl: ");
+    //Serial.println(W_encL);
     //Serial.println(PWM);
     Wl_ant = salL;
     Err_Wl_ant = Err_Wl;
+
     Wr += W_encR;
     Wl += W_encL;
-
-    Wp_MPU = Wp_MPU / (carControlPeriod / motorControlPeriod);
-    Vp_MPU = Vp_MPU / (carControlPeriod / motorControlPeriod);
-
-    float Vel_X_MPU = cos(theta_MPU[cont - 1] * 0.0174532925) * Vp_MPU;
-    float Vel_Y_MPU = sin(theta_MPU[cont - 1] * 0.0174532925) * Vp_MPU;
-
-    dt = (millis() - prevTimeMPU) / 1000.0;
-    prevTimeMPU = millis();
-
-    x_MPU[cont] = x_MPU[cont - 1] + dt * Vel_X_MPU * 6.41025641;
-    y_MPU[cont] = y_MPU[cont - 1] + dt * Vel_Y_MPU * 6.41025641;
-    theta_MPU[cont] = theta_MPU[cont - 1] + dt * Wp_MPU;
-
-    Serial.print("Wr: ");
-    Serial.print(Wr);
-    Serial.print("  Vp: ");
-    Serial.print(Vp_MPU);
-    Serial.print("  VelX: ");
-    Serial.print(Vel_X_MPU);
-    Serial.print("  VelY: ");
-    Serial.print(Vel_Y_MPU);
-    Serial.print("  Wp: ");
-    Serial.print(Wp_MPU);
-    Serial.print("  x: ");
-    Serial.print(x_MPU[cont]);
-    Serial.print("  y: ");
-    Serial.print(y_MPU[cont]);
-    Serial.print("  tetaAct: ");
-    Serial.println(theta_MPU[cont]);
-    Vp_MPU = 0;
-    Wp_MPU = 0;
   }
 
   if (in)
@@ -584,11 +764,11 @@ void codeForTask1(void *parameter)
   for (;;)
   {
 
-    while (SerialBT.available())
+    /*while (SerialBT.available())
     {
       int letra = SerialBT.read();
       incoming += (char)letra;
-    }
+    }*/
 
     if (incoming != "")
     {
@@ -733,4 +913,37 @@ float processEnc(bool enc_antA, bool encA, bool enc_antB, bool encB, int now_mic
   }
   float velW = 1666.666 / ((float)dT);
   return velW;
+}
+
+void calibration()
+{
+  for (size_t i = 0; i < 100; i++)
+  {
+    // Read accelerometer data
+    Wire.beginTransmission(MPU);
+    Wire.write(0x3D);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 4, true);
+    AcY = Wire.read() << 8 | Wire.read();
+
+    // Read gyroscope data
+    Wire.beginTransmission(MPU);
+    Wire.write(0x47);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 4, true);
+    GyZ = Wire.read() << 8 | Wire.read();
+
+    Ac_Y = AcY * (9.81 / 16384.0);
+    Gy_Z = GyZ * (250.0 / 32768.0) * (3.14159 / 180);
+
+    dt = (millis() - prevTime) / 1000.0;
+    prevTime = millis();
+
+    Ac_Y_ini += Ac_Y * dt;
+    Gy_Z_ini += Gy_Z * dt;
+
+    delay(10);
+  }
+  Ac_Y_ini = Ac_Y_ini / 100.0;
+  Gy_Z_ini = Gy_Z_ini / 100.0;
 }
